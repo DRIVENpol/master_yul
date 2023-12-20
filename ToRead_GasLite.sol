d364ad5e51f1c02bee95675c82a348fd1121f0f3
Paul <84129217+DRIVENpol@users.noreply.github.com>
2023-12-21 01:56:12 +0200

    Update ToRead_GasLite.sol

--- ToRead_GasLite.sol
+++ ToRead_GasLite.sol
@@ -72,7 +72,6 @@ contract GasliteDrop {
             // If addresses.length = 3, then shl(5, 3) = 3 * 2^5 = 96
             // _addresses.offset - the offset of the addresses array - where it starts in memory (from right to left)
             // add(_addresses.offset, shl(5, _addresses.length)) -> _addresses.offset + _addresses.length * 2^5
-            // So end is the end of the addresses array
             let end := add(_addresses.offset, shl(5, _addresses.length))
             // diff = _addresses.offset - _tokenIds.offset
             let diff := sub(_addresses.offset, _tokenIds.offset)
